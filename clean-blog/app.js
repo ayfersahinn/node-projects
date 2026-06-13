3e176b1122ce25339099df19e5500b55858f25fe
Ayfer Şahin <ayfers547@gmail.com>
2026-06-13 16:06:50 +0300

    add clean-blog static files

--- clean-blog/app.js
+++ clean-blog/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = new express();
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
 });
 
 const port = 3000;
