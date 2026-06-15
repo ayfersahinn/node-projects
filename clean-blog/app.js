6626f9cb668b6d677b7583086c59829adeb6ff20
Ayfer Şahin <ayfers547@gmail.com>
2026-06-15 19:10:55 +0300

    configure EJS, move templates to views, and set up partials

--- clean-blog/app.js
+++ clean-blog/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
 
 const app = new express();
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  // res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
