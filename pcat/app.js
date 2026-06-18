33bc24b26eb63da7815936a2b173ced5e6322d4a
Ayfer Şahin <ayfers547@gmail.com>
2026-06-18 21:03:35 +0300

    connect MongoDB and implement post creation and listing

--- pcat/app.js
+++ pcat/app.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
+const Photo = require("./models/Photo");
 const app = new express();
 
+mongoose.connect("mongodb://localhost:27017/pcat-test-db").then(() => {
+  console.log("MongoDB connected");
+});
 // template engine
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-app.get("/", (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/", async (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "temp/index.html"));   //statik gönderim
-  res.render("index");
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
 app.get("/add-photo", (req, res) => {
   res.render("add-photo");
+});
+
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
 });
 const port = 3000;
 
