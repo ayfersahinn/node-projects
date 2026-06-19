252e7566f605a29e6df0ff0056d137860cb6fc46
Ayfer Şahin <ayfers547@gmail.com>
2026-06-19 21:05:13 +0300

    upload new image

--- pcat/app.js
+++ pcat/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const fileUpload = require("express-fileupload");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const Photo = require("./models/Photo");
+const fs = require("fs");
+
 const app = new express();
 
 mongoose.connect("mongodb://localhost:27017/pcat-test-db").then(() => {
@@ -14,9 +17,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 app.get("/", async (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "temp/index.html"));   //statik gönderim
-  const photos = await Photo.find({});
+  const photos = await Photo.find().sort("-dateCreated");
   res.render("index", {
     photos,
   });
@@ -35,9 +39,25 @@ app.get("/add-photo", (req, res) => {
 });
 
 app.post("/photos", async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect("/");
+  // await Photo.create(req.body);
+  // res.redirect("/");
+  const uploadDir = "public/uploads";
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdir(uploadDir, (err) => {
+      if (err) console.log(err);
+    });
+  }
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadImage.name;
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadImage.name,
+    });
+    res.redirect("/");
+  });
 });
+
 const port = 3000;
 
 app.listen(port, () => {
