9144c2843231b251fd60f3a49233da2f5be1f370
Ayfer Şahin <ayfers547@gmail.com>
2026-06-20 17:53:14 +0300

    Refactor project structure to follow MVC architecture

--- pcat/app.js
+++ pcat/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const path = require("path");
 const fileUpload = require("express-fileupload");
 const ejs = require("ejs");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
-const Photo = require("./models/Photo");
-const fs = require("fs");
-
+const photoController = require("./controllers/photoControllers");
+const pageController = require("./controllers/pageControllers");
 const app = new express();
 
 mongoose.connect("mongodb://localhost:27017/pcat-test-db").then(() => {
@@ -25,66 +23,30 @@ app.use(
   }),
 );
 
-app.get("/", async (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "temp/index.html"));   //statik gönderim
-  const photos = await Photo.find().sort("-dateCreated");
-  res.render("index", {
-    photos,
-  });
-});
+app.get("/", photoController.getAllPhotos);
 
-app.get("/photos/:id", async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render("photo-detail", {
-    photo,
-  });
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add-photo", (req, res) => {
-  res.render("add-photo");
-});
+app.get("/photos/:id", photoController.getPhoto);
+app.post("/photos", photoController.createPhoto);
+app.put("/photos/:id", photoController.updatePhoto);
+app.delete("/photos/:id", photoController.deletePhoto);
 
-app.post("/photos", async (req, res) => {
-  // await Photo.create(req.body);
-  // res.redirect("/");
-  const uploadDir = "public/uploads";
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdir(uploadDir, (err) => {
-      if (err) console.log(err);
-    });
-  }
-  let uploadImage = req.files.image;
-  let uploadPath = __dirname + "/public/uploads/" + uploadImage.name;
-  uploadImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: "/uploads/" + uploadImage.name,
-    });
-    res.redirect("/");
-  });
-});
-app.get("/photos/edit/:id", async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render("edit-photo", {
-    photo,
-  });
-});
-app.put("/photos/:id", async (req, res) => {
-  await Photo.findByIdAndUpdate(req.params.id, req.body);
-  res.redirect(`/photos/${req.params.id}`);
-});
+app.get("/about", pageController.getAboutPage);
+app.get("/add-photo", pageController.getAddPage);
 
-app.delete("/photos/:id", async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + "/public" + photo.image;
-  fs.unlinkSync(deletedImage);
-  await Photo.findByIdAndDelete(req.params.id);
-  res.redirect("/");
-});
+app.get("/photos/edit/:id", pageController.getEditPage);
+
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda çalışıyor.`);
 });
+
+// Request
+//    ↓
+// Route
+//    ↓
+// Controller
+//    ↓
+// Model (MongoDB)
+//    ↓
+// Response (EJS / Redirect)
---
+++ pcat/controllers/pageControllers.js
@@ -0,0 +1,15 @@
+const Photo = require("../models/Photo");
+exports.getAboutPage = (req, res) => {
+  res.render("about");
+};
+
+exports.getAddPage = (req, res) => {
+  res.render("add-photo");
+};
+
+exports.getEditPage = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render("edit-photo", {
+    photo,
+  });
+};
