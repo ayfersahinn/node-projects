4d368156bba6200fd58a7b10ccaaa075683aa179
Ayfer Şahin <ayfers547@gmail.com>
2026-06-21 19:51:29 +0300

    Implement edit page and update route

--- clean-blog/app.js
+++ clean-blog/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const path = require("path");
 const ejs = require("ejs");
 const Post = require("./models/Post");
@@ -13,7 +14,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  }),
+);
 app.get("/", async (req, res) => {
   // res.sendFile(path.resolve(__dirname, "temp/index.html"));
   const posts = await Post.find({});
@@ -37,6 +42,17 @@ app.get("/add_post", (req, res) => {
 app.post("/posts", async (req, res) => {
   await Post.create(req.body);
   res.redirect("/");
+});
+
+app.get("/posts/edit/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("edit", {
+    post,
+  });
+});
+app.put("/posts/:id", async (req, res) => {
+  await Post.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect(`/posts/${req.params.id}`);
 });
 const port = 3000;
 app.listen(port, () => {
--- clean-blog/models/Post.js
+++ clean-blog/models/Post.js
@@ -1,26 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  name: String,
-  message: String,
-  createdDate: {
-    type: Date,
-    default: Date.now,
+const postSchema = new Schema(
+  {
+    name: String,
+    message: String,
   },
-});
-postSchema.virtual("formattedDate").get(function () {
-  return new Date(this.createdDate).toLocaleString("tr-TR", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-});
-
-postSchema.set("toJSON", { virtuals: true });
-postSchema.set("toObject", { virtuals: true });
+  {
+    timestamps: true,
+  },
+);
 
 const Post = mongoose.model("Post", postSchema);
 module.exports = Post;
