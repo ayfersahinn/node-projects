cbe3947fc2716e78a364fe3d775e351d07936184
Ayfer Şahin <ayfers547@gmail.com>
2026-06-21 20:13:17 +0300

    MVC structure improvements

--- clean-blog/app.js
+++ clean-blog/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const path = require("path");
 const ejs = require("ejs");
 const Post = require("./models/Post");
+const PostController = require("./controllers/PostController");
+const PageController = require("./controllers/PageControllers");
 
 const app = new express();
+
+// db connection
 mongoose
   .connect("mongodb://localhost:27017/cleanblog-test-db")
   .then(() => console.log("connected mongodb"))
   .catch((err) => console.log(err));
+
+//middlewares
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -19,46 +24,17 @@ app.use(
     methods: ["POST", "GET"],
   }),
 );
-app.get("/", async (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "temp/index.html"));
-  const posts = await Post.find({});
-  res.render("index", {
-    posts,
-  });
-});
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post", {
-    post,
-  });
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
-});
 
-app.post("/posts", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
-});
+app.get("/", PostController.getAllPosts);
+app.get("/posts/:id", PostController.getPost);
+app.post("/posts", PostController.createPost);
+app.put("/posts/:id", PostController.updatePost);
+app.delete("/posts/:id", PostController.deletePost);
 
-app.get("/posts/edit/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("edit", {
-    post,
-  });
-});
-app.put("/posts/:id", async (req, res) => {
-  await Post.findByIdAndUpdate(req.params.id, req.body);
-  res.redirect(`/posts/${req.params.id}`);
-});
+app.get("/about", PageController.getAboutPage);
+app.get("/add_post", PageController.getAddPage);
+app.get("/posts/edit/:id", PageController.getEditPage);
 
-app.delete("/posts/:id", async (req, res) => {
-  await Post.findByIdAndDelete(req.params.id);
-  res.redirect("/");
-});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda çalışıyor.`);
