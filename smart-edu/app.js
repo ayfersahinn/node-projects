e25b3272de647e258046ea73e4c10abe8f98988a
Ayfer Şahin <ayfers547@gmail.com>
2026-06-23 21:29:57 +0300

    Initialize SmartEdu project with basic routing and server setup

---
+++ smart-edu/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda çalışıyor`);
+});
