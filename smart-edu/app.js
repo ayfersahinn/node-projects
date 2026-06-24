0f83b34323e9502705a7fa0066a668e9d537035e
Ayfer Şahin <ayfers547@gmail.com>
2026-06-24 19:07:18 +0300

    display courses fetched from database

--- smart-edu/app.js
+++ smart-edu/app.js
@@ -11,6 +11,8 @@ mongoose
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
