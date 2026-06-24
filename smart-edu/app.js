ea23fb854b32cc08d5933aae3cf4aa26bba658bd
Ayfer Şahin <ayfers547@gmail.com>
2026-06-24 17:57:16 +0300

    implement course module with schema, routes, and controller

--- smart-edu/app.js
+++ smart-edu/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoutes");
+const courseRoute = require("./routes/courseRoutes");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/smartedu-db")
+  .then(() => console.log("connected mongodb"))
+  .catch((err) => console.log(err));
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 const port = 3000;
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda çalışıyor`);
