5777083d4d74d29f10a2cf0ec787568769f91be6
Ayfer Şahin <ayfers547@gmail.com>
2026-06-25 15:05:15 +0300

    add course filtering by category

--- smart-edu/models/Category.js
+++ smart-edu/models/Category.js
@@ -1,25 +1,18 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+
 const Schema = mongoose.Schema;
 
-const CourseSchema = new Schema(
+const CategorySchema = new Schema(
   {
     name: {
       type: String,
       unique: true,
       required: true,
     },
-    description: {
-      type: String,
-      required: true,
-    },
     slug: {
       type: String,
       unique: true,
-    },
-    category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
     },
   },
   {
@@ -27,12 +20,12 @@ const CourseSchema = new Schema(
   },
 );
 
-CourseSchema.pre("save", function () {
+CategorySchema.pre("save", function () {
   this.slug = slugify(this.name, {
     lower: true,
     strict: true,
   });
 });
 
-const Course = mongoose.model("Course", CourseSchema);
-module.exports = Course;
+const Category = mongoose.model("Category", CategorySchema);
+module.exports = Category;
