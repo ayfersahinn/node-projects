aea58720e322c1e4e887ef899d6d4af259e7daea
Ayfer Şahin <ayfers547@gmail.com>
2026-06-25 16:24:52 +0300

    implement user login authentication

--- smart-edu/controllers/authController.js
+++ smart-edu/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -7,6 +7,32 @@ exports.createUser = async (req, res) => {
       status: "success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: error.message,
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).send("User not found");
+    }
+
+    const same = await bcrypt.compare(password, user.password);
+
+    if (!same) {
+      return res.status(400).send("Wrong password");
+    }
+
+    res.status(200).send("You are logged in");
   } catch (error) {
     res.status(400).json({
       status: "failed",
